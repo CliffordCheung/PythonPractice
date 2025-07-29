685361682a90b6d55778127917d8220d6e4f725e
CliffordCheung <cliffordcheung77@gmail.com>
2025-07-29 22:58:05 +0800

    Progress add till ch5

---
+++ Ch4_Operations2/python5_1.py
@@ -0,0 +1,14 @@
+# Initialize variables
+has_license = True
+has_experience = False
+has_clean_record = True
+
+# Calculate conditions
+can_drive_car = (has_license and has_clean_record)
+can_drive_truck = (has_license and has_experience and has_clean_record)
+cannot_drive_any = (not has_license or not has_clean_record)
+
+# Don't delete the lines below
+print("Can drive car:", can_drive_car)
+print("Can drive truck:", can_drive_truck)
+print("Cannot drive any:", cannot_drive_any)
